19fe64d8193159ce13a1efb91180665e0a4f1d4b
Arlo Godfrey <arlo.godfrey@outlook.com>
2021-08-06 15:25:36 +0000

    Set up category sorting mode selection logic

--- LaunchShowcase.Sdk/Services/LaunchScoringCategory.cs
+++ LaunchShowcase.Sdk/Services/LaunchScoringCategory.cs
@@ -1,39 +1,47 @@
-﻿namespace LaunchShowcase.Sdk.Services
+﻿using System;
+
+namespace LaunchShowcase.Sdk.Services
 {
     /// <summary>
     /// The categories that a project is judged on for the Launch event.
     /// </summary>
+    [Flags]
     public enum LaunchScoringCategory
     {
         /// <summary>
+        /// No selected category.
+        /// </summary>
+        None,
+
+        /// <summary>
         /// A Fluent Design principle. Responsive layouts, adaptive input methods, and a natural UI fit on every device.
         /// </summary>
-        Flexibility,
+        Flexibility = 1,
 
         /// <summary>
         /// A Fluent Design principle. The user experience is stable, intuitive, and delightful to use.
         /// </summary>
-        Empathy,
+        Empathy = 2,
 
         /// <summary>
         /// A Fluent Design principle. The app is engaging and immersive. Shadows, animations, depth, general design.
         /// </summary>
-        Beauty,
+        Beauty = 4,
 
         /// <summary>
         /// Apps are built on a great idea and have the potential to grow into something even bigger.
         /// </summary>
-        Potential,
+        Potential = 8,
 
         /// <summary>
         /// Apps that do something very original, unique, or uncommon, or serves an unfilled niche. Yugen Mosaic is a good example of this.
         /// </summary>
-        Originality,
+        Originality = 16,
 
         /// <summary>
         /// Screen reader support, localization, high contrast themes, and keyboard navigation make your app more accessible to users around the world.
         /// </summary>
-        Accessiblity,
+        Accessiblity = 32,
     }
 
 }
--- LaunchShowcase.Sdk/ViewModels/MainViewModel.cs
+++ LaunchShowcase.Sdk/ViewModels/MainViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using LaunchShowcase.Sdk.Services;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
 using OwlCore.Extensions;
 using OwlCore.Provisos;
 using System.Collections.Generic;
@@ -11,27 +13,23 @@ namespace LaunchShowcase.Sdk.ViewModels
     /// <summary>
     /// The root view model that holds all data used by the application.
     /// </summary>
-    public class MainViewModel : IAsyncInit
+    public class MainViewModel : ObservableObject, IAsyncInit
     {
+        private const int LAUNCH_YEAR = 2021;
+
         private readonly CommunityBackendService _backendService = CommunityBackendService.Instance;
+        private List<ProjectViewModel> _unsortedLaunchProjects;
+        private LaunchScoringCategory _sortingMode;
 
-        /// <summary>
-        /// The year to use when retrieving launch projects.
-        /// </summary>
-        public const int LaunchYear = 2021;
-
-        /// <summary>
-        /// Singleton instance.
-        /// </summary>
         public static MainViewModel Instance { get; set; } = new MainViewModel();
 
         public MainViewModel()
         {
+            _unsortedLaunchProjects = new List<ProjectViewModel>();
             LaunchProjects = new ObservableCollection<ProjectViewModel>();
-        }
 
-        /// <inheritdoc/>
-        public bool IsInitialized { get; private set; }
+            ToggleProjectsSortingModeCommand = new RelayCommand<LaunchScoringCategory>(ToggleProjectsSortingMode);
+        }
 
         /// <inheritdoc/>
         public Task InitAsync()
@@ -41,14 +39,37 @@ namespace LaunchShowcase.Sdk.ViewModels
             return PopulateLaunchProjects();
         }
 
+        /// <inheritdoc/>
+        public bool IsInitialized { get; private set; }
+
         /// <summary>
         /// All projects participating in the event this year.
         /// </summary>
         public ObservableCollection<ProjectViewModel> LaunchProjects { get; set; }
 
+        public IRelayCommand<LaunchScoringCategory> ToggleProjectsSortingModeCommand { get; }
+
+        public LaunchScoringCategory SortingMode
+        {
+            get => _sortingMode;
+            set => SetProperty(ref _sortingMode, value);
+        }
+
+        public bool HasFlexibilitySortingMode => (SortingMode & LaunchScoringCategory.Flexibility) == LaunchScoringCategory.Flexibility;
+
+        public bool HasEmpathySortingMode => (SortingMode & LaunchScoringCategory.Empathy) == LaunchScoringCategory.Empathy;
+
+        public bool HasBeautySortingMode => (SortingMode & LaunchScoringCategory.Beauty) == LaunchScoringCategory.Beauty;
+
+        public bool HasPotentialSortingMode => (SortingMode & LaunchScoringCategory.Potential) == LaunchScoringCategory.Potential;
+
+        public bool HasOriginalitySortingMode => (SortingMode & LaunchScoringCategory.Originality) == LaunchScoringCategory.Originality;
+
+        public bool HasAccessiblitySortingMode => (SortingMode & LaunchScoringCategory.Accessiblity) == LaunchScoringCategory.Accessiblity;
+
         public async Task PopulateLaunchProjects()
         {
-            var projectsRes = await _backendService.ProjectsService.GetLaunchProjects(LaunchYear);
+            var projectsRes = await _backendService.ProjectsService.GetLaunchProjects(LAUNCH_YEAR);
 
             await projectsRes.Projects.InParallel(async project =>
             {
@@ -57,9 +78,26 @@ namespace LaunchShowcase.Sdk.ViewModels
 
                 if (projectVm.HasMinimumInfoForLaunchShowcase())
                 {
+                    _unsortedLaunchProjects.Add(projectVm);
                     LaunchProjects.Add(projectVm);
                 }
             });
+        }
+
+        private void ToggleProjectsSortingMode(LaunchScoringCategory category)
+        {
+            SortingMode ^= category;
+            UpdateHasSortingModeInpc();
+        }
+
+        private void UpdateHasSortingModeInpc()
+        {
+            OnPropertyChanged(nameof(HasFlexibilitySortingMode));
+            OnPropertyChanged(nameof(HasEmpathySortingMode));
+            OnPropertyChanged(nameof(HasBeautySortingMode));
+            OnPropertyChanged(nameof(HasPotentialSortingMode));
+            OnPropertyChanged(nameof(HasOriginalitySortingMode));
+            OnPropertyChanged(nameof(HasAccessiblitySortingMode));
         }
     }
 }
