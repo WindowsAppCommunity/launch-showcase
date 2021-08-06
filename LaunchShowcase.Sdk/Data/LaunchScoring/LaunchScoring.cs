8349e8977920dc606444e298a3a6cc377f542d3a
Arlo Godfrey <arlo.godfrey@outlook.com>
2021-08-06 14:39:57 +0000

    Added comment

--- LaunchShowcase.Sdk/Data/LaunchScoring/LaunchScoring.cs
+++ LaunchShowcase.Sdk/Data/LaunchScoring/LaunchScoring.cs
@@ -9,7 +9,7 @@ namespace LaunchShowcase.Sdk.Data.LaunchScoring
         /// The score for each project in each category. The inner dictionary is keyed by project's ID, the value is compound score from all judges.
         /// </summary>
         /// <remarks>
-        /// We aren't storing or pulling this data from the backend.
+        /// We aren't storing or pulling this data from the backend due to time constraints, but it will eventually need to be moved there.
         /// We could have used JSON, but there's no reason to add deserialization weight.
         /// </remarks>
         public static Dictionary<LaunchScoringCategory, Dictionary<int, int>> Scoring { get; } = new Dictionary<LaunchScoringCategory, Dictionary<int, int>>
