a38292f54e926dc94b3613628106588d90d05cb9
nerocui <nerocui@outlook.com>
2021-08-28 20:18:12 -0700

    added custom flipview for jithub's showcase

--- LaunchShowcase.Shared/TemplateSelectors/ShowcasePageTemplateSelector.cs
+++ LaunchShowcase.Shared/TemplateSelectors/ShowcasePageTemplateSelector.cs
@@ -37,7 +37,6 @@ namespace LaunchShowcase.TemplateSelectors
                 // and should match the x:Key given to the data template.
                 LaunchProjects.Archon => GetTemplateByName("DefaultShowcaseTemplate"), 
                 LaunchProjects.JitHub => GetTemplateByName("JitHubShowcaseTemplate"),
-                LaunchProjects.FluentStore => GetTemplateByName("FluentStore"),
                 LaunchProjects.TranslucentTB => GetTemplateByName("TranslucentTBShowcaseTemplate"),
                 _ => GetTemplateByName("DefaultShowcaseTemplate"),
             };
---
+++ LaunchShowcase.Shared/Themes/ShowcaseTemplates/JitHubControls/DesignControl.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace LaunchShowcase.Themes.ShowcaseTemplates.JitHubControls
+{
+    public sealed partial class DesignControl : UserControl
+    {
+        public DesignControl()
+        {
+            this.InitializeComponent();
+            var images = new List<ImageSource>()
+            {
+                new BitmapImage(new Uri("https://nerocui.files.wordpress.com/2021/08/repo-1.png")),
+                new BitmapImage(new Uri("https://nerocui.files.wordpress.com/2021/08/commits.png")),
+                new BitmapImage(new Uri("https://nerocui.files.wordpress.com/2021/08/commits-1.png")),
+                new BitmapImage(new Uri("https://nerocui.files.wordpress.com/2021/08/home.png")),
+                new BitmapImage(new Uri("https://nerocui.files.wordpress.com/2021/08/home-1.png")),
+                new BitmapImage(new Uri("https://nerocui.files.wordpress.com/2021/08/issues.png")),
+                new BitmapImage(new Uri("https://nerocui.files.wordpress.com/2021/08/issues-1.png")),
+                new BitmapImage(new Uri("https://nerocui.files.wordpress.com/2021/08/issues-2.png")),
+                new BitmapImage(new Uri("https://nerocui.files.wordpress.com/2021/08/new_issue.png")),
+                new BitmapImage(new Uri("https://nerocui.files.wordpress.com/2021/08/no_issues_found.png")),
+                new BitmapImage(new Uri("https://nerocui.files.wordpress.com/2021/08/repo.png")),
+                new BitmapImage(new Uri("https://nerocui.files.wordpress.com/2021/08/repo-1.png")),
+                new BitmapImage(new Uri("https://nerocui.files.wordpress.com/2021/08/commits.png")),
+            };
+            MyContent.Children.Add(new JitHubFlipView(images));
+        }
+    }
+}
